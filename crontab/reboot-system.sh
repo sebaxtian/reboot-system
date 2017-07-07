072ab5408c8b902d305d3f3fe517fee9713ada42
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-07-07 15:12:43 -0500

    Agrego configuracion para ejecutar desde Crontab Linux

---
+++ crontab/reboot-system.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script para ejecutar aplicacion Node reboot-system
+#
+
+# 1. Carga el PATH donde se encuentra reboot-system
+cd /path/to/node/reboot-system
+# 2. Ejecuta la aplicacion de Node reboot-system
+node app.js $1
